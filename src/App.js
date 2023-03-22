da33ca657d2e7f77e10ee689078746383aee7ea9
Mateusz Grincewicz <matek160393@gmail.com>
2023-03-22 06:34:26 +0100

    typo fix

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       {
         content,
         done: false,
-        id: tasks.lenght ? tasks[tasks.lenght - 1].id + 1 : 1,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
       },
     ]);
   };
